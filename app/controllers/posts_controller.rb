a13759ce92670762652b8fcce18eaac8e1a60fd1
Hoon Lee <yl.hoony@gmail.com>
2018-01-23 20:05:11 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    raise params.inspect
+    # raise params.inspect
+    @post = Post.find(params[:id])
+    @post.update(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
   end
 end
